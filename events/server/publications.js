412792122e94cfb0a405e938a9c497f35913b9c9
Andrew Dai <theman@andrewdai.co>
2016-02-27 12:29:52 -0800

    move userdata publication to root level

--- events/server/publications.js
+++ events/server/publications.js
@@ -1,20 +1,3 @@
 Meteor.publish("Events", function(){
   return Events.find({});
 });
-
-Meteor.publish("userData", function () {
-  if (this.userId) {
-    return Meteor.users.find(
-      {
-        _id: this.userId
-      },
-      {
-        fields: {
-          'services': 1
-        }
-      }
-    );
-  } else {
-    this.ready();
-  }
-});
---
+++ server/publications.js
@@ -0,0 +1,16 @@
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find(
+      {
+        _id: this.userId
+      },
+      {
+        fields: {
+          'services': 1
+        }
+      }
+    );
+  } else {
+    this.ready();
+  }
+});
