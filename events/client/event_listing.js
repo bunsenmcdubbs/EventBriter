8717aff25423b942f3a0ee07c9aae45eb9235a1d
Andrew Dai <theman@andrewdai.co>
2016-02-28 19:16:40 -0700

    sort event listing by date, utilize bootstrap for appearance

--- events/client/event_listing.js
+++ events/client/event_listing.js
@@ -1,18 +1,5 @@
-
 Template.event_listing.helpers({
-  create: function(){
-
-  },
-  rendered: function(){
-
-  },
   event_list: function() {
-    return Events.find({});
-  }
-});
-
-Template.event_listing.events({
-  "click #foo": function(event, template){
-
+    return Events.find({}, {sort: ['start', 'end']});
   }
 });
